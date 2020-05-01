c5ceb374b1d0871ffe4a4059310d7ee69e9aa02a
komalahire <komala18@navgurukul.org>
2020-05-01 11:59:24 +0530

    first commit

---
+++ src/components/Homepage.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import { Button, TextField } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+
+import axios from 'axios'
+
+export class Homepage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      Text: "",
+      data:''
+    };
+  }
+
+  handleChange = e =>{
+    const regex = /[A-Za-z]/
+    if ((e.target.value === '')|| regex.test(e.target.value)){
+        this.setState({text:e.target.value})
+    }
+}
+
+handleSubmit = async e  => {
+    e.preventDefault();
+    const response = await axios.get('https://restcountries.eu/rest/v2/name/'+ this.state.text,{
+
+    })
+    console.log(response)
+    this.setState({data:response.data})
+}
+
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField
+            id="outlined-secondary"
+            label="search..."
+            className={classes.input}
+            variant="outlined"
+            color="secondary"
+            onChange={this.handleChange}
+          />
+          <Button
+                variant='contained'
+                color='primary'
+                className={classes.btn}
+                onClick={this.handleSubmit}
+                disable={!this.state.text}
+                >Submit
+            </Button>
+        </form>
+       
+      </div>
+    );
+  }
+}
+
+export default withStyles({
+  root: {
+    marginLeft:'25%',
+    marginTop: "2%",
+    
+  },
+  input:{
+        width:'50%'
+  },
+
+  btn:{
+      marginTop:"5px",
+      marginLeft:'10px',
+     
+    
+      
+  }
+})(Homepage);
